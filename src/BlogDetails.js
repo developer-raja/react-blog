52000750b1dfb352dbe7734c99f0e3282678358c
Raja <dev@guvi.in>
2021-07-28 11:42:19 +0530

    Hosted json-server

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,32 +1,42 @@
-import { useHistory, useParams } from "react-router-dom";
+import { useHistory, useParams } from 'react-router-dom';
 import useFetch from './useFetch';
 const BlogDetails = () => {
-    const { id } = useParams()
-    const { data:blogs,isloading,error } = useFetch('http://localhost:8000/blogs/'+id)
-    const history = useHistory();
+  const { id } = useParams();
+  const {
+    data: blogs,
+    isloading,
+    error,
+  } = useFetch(
+    'https://my-json-server.typicode.com/developer-raja/json-server/' + id
+  );
+  const history = useHistory();
 
-    const handleClick = () => {
-        fetch('http://localhost:8000/blogs/'+ blogs.id,{
-            method:'DELETE'
-        }).then(() => {
-            history.push('/')
-        })
-    }
+  const handleClick = () => {
+    fetch(
+      'https://my-json-server.typicode.com/developer-raja/json-server/' +
+        blogs.id,
+      {
+        method: 'DELETE',
+      }
+    ).then(() => {
+      history.push('/');
+    });
+  };
 
-    return ( 
-        <div className="blog-details">
-            {isloading && <div>Loading...</div>}
-            {error && <div>{error}</div>}
-            {blogs && (
-                <article>
-                    <h2>{blogs.title}</h2>
-                    <p>Written by {blogs.author}</p>
-                    <div>{blogs.body}</div>
-                    <button onClick={handleClick}>delete</button>
-                </article>
-            )}
-        </div>
-     );
-}
- 
+  return (
+    <div className='blog-details'>
+      {isloading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blogs && (
+        <article>
+          <h2>{blogs.title}</h2>
+          <p>Written by {blogs.author}</p>
+          <div>{blogs.body}</div>
+          <button onClick={handleClick}>delete</button>
+        </article>
+      )}
+    </div>
+  );
+};
+
 export default BlogDetails;
